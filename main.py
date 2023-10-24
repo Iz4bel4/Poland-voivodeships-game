4ca57bad5513d6657e7a0fb20385c655719feaf3
Izabela Szczecinska <izabelaszczecinska5@gmail.com>
2023-10-24 13:17:05 +0200

    checking if a player answer is among 16 voivodeships and writing them onto the map if correct

--- main.py
+++ main.py
@@ -8,11 +8,24 @@ image = 'poland-map.gif'
 screen.addshape(image)
 turtle.shape(image)
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+
+data = pandas.read_csv('16_voivodeships.csv')
+all_voivodeships = data['voivodeship'].to_list()
 
 
-turtle.onscreenclick(get_mouse_click_coor)
+player_answer = screen.textinput(title='x/16 Voivodeships Correct', prompt= 'What is another voivodeship name?')
 
+if player_answer in all_voivodeships:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    voivodeship_data = data[data.voivodeship == player_answer]
+    t.goto(int(voivodeship_data.x), int(voivodeship_data.y))
+    t.write(player_answer,  font=("Arial", 16, "normal"))
 
 turtle.mainloop()
